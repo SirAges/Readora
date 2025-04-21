a56d3da0d9ff89d18a78fc03ba18a6009e44568d
Ekele Stephen <70183403+SirAges@users.noreply.github.com>
2025-04-21 08:36:40 +0100

    Update AdminHome.tsx

--- src/components/admin/AdminHome.tsx
+++ src/components/admin/AdminHome.tsx
@@ -1,14 +1,10 @@
 "use client";
-import { useAuth } from "@/hooks/useAuth";
-import { redirect } from "next/navigation";
-
 const AdminHome = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const { isSignedIn } = useAuth();
-  if (!isSignedIn) redirect("/auth/sign-in");
+  
   return <div className="w-full flex flex-col min-h-screen ">{children}</div>;
 };
 export default AdminHome;
